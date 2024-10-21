3a2dc8f44bdd9f9a79feaa45dffa41142aca5291
matheus3074 <167218113+matheus3074@users.noreply.github.com>
2024-10-21 19:49:40 -0300

    Add files via upload

--- prefabs/bordas.gd
+++ prefabs/bordas.gd
@@ -4,7 +4,8 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
+	var mouse_texture = preload("res://assets/icons/ponteiro_50x50-removebg-preview.png")
+	Input.set_custom_mouse_cursor(mouse_texture)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,3 +14,15 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://prefabs/browser.tscn")
+	
+
+
+func _on_discord_pressed() -> void:
+	get_tree().change_scene_to_file("res://prefabs/discord.tscn")
+
+
+func _on_browser_area_pressed() -> void:
+	get_tree().change_scene_to_file("res://prefabs/browser.tscn")
+
+func _on_discord_area_pressed() -> void:
+	get_tree().change_scene_to_file("res://prefabs/discord.tscn")
--- prefabs/browser.gd
+++ prefabs/browser.gd
@@ -4,7 +4,8 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
+	var mouse_texture = preload("res://assets/icons/ponteiro_50x50-removebg-preview.png")
+	Input.set_custom_mouse_cursor(mouse_texture)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,3 +14,11 @@ func _process(delta: float) -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://prefabs/tile_screen.tscn")
+
+
+func _on_discord_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://prefabs/discord.tscn")
